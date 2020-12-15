37934c51a22e4385a4234db8326b70eb88689852
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-12-15 10:23:44 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './styles.css';
+import LoadButton from './JS/loadButton';
+import refs from './JS/refs';
+import apiService from './JS/apiService';
+import createMarkup from './JS/create-markup';
+// import { data } from 'autoprefixer';
+
+const loadingButton = new LoadButton('button[data-action="load-more"]');
+refs.searchForm.addEventListener('submit', searchSubmit);
+loadingButton.refs.button.addEventListener('click', nextLoadImg);
+
+function searchSubmit(event) {
+  event.preventDefault();
+
+  const inputPlace = event.currentTarget;
+  apiService.query = inputPlace.elements.query.value;
+
+  clearGallery();
+  apiService.resetPage();
+  inputPlace.reset();
+  nextLoadImg();
+}
+function nextLoadImg() {
+  loadingButton.disable();
+
+  apiService.fetchImages().then(image => {
+    createMarkup(image);
+    loadingButton.show();
+    loadingButton.enable();
+
+    window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      behavior: 'smooth',
+    });
+  });
+}
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
